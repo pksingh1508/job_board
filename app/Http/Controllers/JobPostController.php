0903b0e4939aa6a23ba2e50c1058ccde37ee96f3
pksingh1508 <rajiit1212@gmail.com>
2026-01-30 13:41:52 +0530

    Build a layout using components

--- app/Http/Controllers/JobPostController.php
+++ app/Http/Controllers/JobPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobPost;
 use Illuminate\Http\Request;
 
 class JobPostController extends Controller
@@ -11,7 +12,7 @@ class JobPostController extends Controller
      */
     public function index()
     {
-        //
+        return view('jobs.index', ['jobs' => JobPost::all()]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\JobPostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('jobs', JobPostController::class)
+    ->only('index');
