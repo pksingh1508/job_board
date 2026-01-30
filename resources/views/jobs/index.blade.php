2651153226fa55f64368875a98d29112937bb111
pksingh1508 <rajiit1212@gmail.com>
2026-01-30 22:39:57 +0530

    add custom input field

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,4 +1,26 @@
 <x-layout>
+    @php
+        $breadcrumbLinks = ['Jobs' => '#'];
+    @endphp
+    <x-bread-crumbs class="my-4 mx-2" :links="$breadcrumbLinks" />
+
+    {{-- filter goes here --}}
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                <x-text-input name="search" value="" placeholder="Search for any text" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+                <div class="flex space-x-2">
+                    <x-text-input name="min_salary" value="" placeholder="From" />
+                    <x-text-input name="max_salary" value="" placeholder="To" />
+                </div>
+            </div>
+        </div>
+    </x-card>
+
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :$job>
             <div class="mt-5">
